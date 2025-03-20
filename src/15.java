823b8971b911e73c5e8c8569c36848caac43cd8d
cloutiersulentic695 <cloutiersulentic695@gmail.com>
2025-03-20 12:23:58 +0000

    Add 15.java

---
+++ src/15.java
@@ -0,0 +1,8 @@
+public class SchoolProject {
+    public static void main(String[] args) {
+        // Randomly select an element from the array
+        int randomNumber = (int)(Math.random() * 10 + 1);
+        
+        System.out.println("Random Number: " + randomNumber);
+    }
+}
